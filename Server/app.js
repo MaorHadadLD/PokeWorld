7b371efade5441483570754bc90c16a72fb7d2eb
MaorHadadLD <maorhadad94@gmail.com>
2025-06-05 16:42:01 +0300

    Adding postRuter to the app.js

--- Server/app.js
+++ Server/app.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
+const postRoutes = require('./routes/postRoutes');
 
 dotenv.config();
 connectDB();
@@ -11,11 +12,14 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 app.use('/api/users', userRoutes);
+app.use('/api/posts', postRoutes);
 
 app.get('/', (req,res) => {
     res.send('Api is running...');
 });
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
