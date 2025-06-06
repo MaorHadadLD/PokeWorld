614f6660a7b486b911f60e3461a466189ba0efa2
MaorHadadLD <maorhadad94@gmail.com>
2025-06-06 23:45:57 +0300

    Adding updateComment func

--- Server/controllers/commentController.js
+++ Server/controllers/commentController.js
@@ -17,6 +17,31 @@ const getSingleComment = async (req, res) => {
     }
 };
 
+const updateComment = async (req, res) => {
+    const { postId, commentId} = req.params;
+    const { text } = req.body;
+
+    try {
+        const post = await Post.findById(postId);
+        const comment = post.comments.id(commentId);
+        if (!comment) {
+            return res.status(404).json({message: 'Comment not found'});
+        }
+
+        if(comment.user.toString() !== req.user._id.toString()) {
+            return res.status(403).json({message: 'You are not authorized to update this comment'});
+        }
+
+        comment.text = text;
+        await post.save();
+
+        res.status(200).json({message: 'Comment updated successfully', comment});
+    } catch (err) {
+        res.status(500).json({message: 'Server error', error: err.message});
+    }
+};
+
 module.exports = {
     getSingleComment,
+    updateComment,
 }
