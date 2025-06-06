2c33a533a68e7b672b18ae8b77843cafd4c137d1
MaorHadadLD <maorhadad94@gmail.com>
2025-06-06 23:47:43 +0300

    Adding updateComment to the rute

--- Server/routes/commentRoutes.js
+++ Server/routes/commentRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { getSingleComment } = require('../controllers/commentController');
+const { getSingleComment, updateComment } = require('../controllers/commentController');
 const { protect } = require('../middleware/authMiddleware');
 
 router.get('/:postId/:commentId', protect, getSingleComment);
+router.put('/:postId/:commentId', protect, updateComment);
 
 module.exports = router;
