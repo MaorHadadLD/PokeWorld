a37044c7106c1915ac66fd708b3eef82f577b4cb
MaorHadadLD <maorhadad94@gmail.com>
2025-06-06 23:55:13 +0300

    Updating the commentRoutes

--- Server/routes/commentRoutes.js
+++ Server/routes/commentRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getSingleComment, updateComment } = require('../controllers/commentController');
+const { getSingleComment, updateComment, deleteComment } = require('../controllers/commentController');
 const { protect } = require('../middleware/authMiddleware');
 
 router.get('/:postId/:commentId', protect, getSingleComment);
 router.put('/:postId/:commentId', protect, updateComment);
+router.delete('/:postId/:commentId', protect, deleteComment);
 
 module.exports = router;
