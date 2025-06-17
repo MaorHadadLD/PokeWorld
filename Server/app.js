7a67ff117754cb97b92f35ab8b74d7565aaafaca
MaorHadadLD <maorhadad94@gmail.com>
2025-06-17 13:19:44 +0300

    Updating app.js

--- Server/app.js
+++ Server/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const server = require('http').createServer(app);
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
 const postRoutes = require('./routes/postRoutes');
 const commentRoutes = require('./routes/commentRoutes');
+const io = require('socket.io')(server, {
+    cors: {
+        origin: '*'
+    }
+})
 
 dotenv.config();
 connectDB();
@@ -21,5 +27,6 @@ app.use('/api/comments', commentRoutes);
 app.get('/', (req,res) => {
     res.send('Api is running...');
 });
+app.set('io', io);
 
 module.exports = app;
