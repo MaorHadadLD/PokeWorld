51c0c7a80df36e28daed0136bbbe13b7e4480eb9
MaorHadadLD <maorhadad94@gmail.com>
2025-06-19 01:51:15 +0300

    Adding LoginPage

--- client/api/user.js
+++ client/api/user.js
@@ -1,17 +1,16 @@
 import axios from 'axios';
 
-const loginUser = async (req, res) => {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
+const API_URL = 'http://localhost:5000/api/users';
 
-    if (!user || !(await user.matchPassword(password))) {
-        return res.status(401).json({ message: 'Invalid email or password' });
-    }
+export const loginUser = async (email, password) => {
+    const response = await axios.post(`${API_URL}/login`, { email, password });
 
-    res.json({
-        _id: user._id,
-        username: user.username,
-        email: user.email,
-        token: generateToken(user._id),
-    });
-}
+    return response.data;
+};
+
+export const registerUser = async (username, email, password) => {
+    const response = await axios.post(`${API_URL}/register`, { username, email, password });
+
+    return response.data;
+};
+
---
+++ client/src/pages/LoginPage.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {loginUser} from '../../api/user';
+
+const LoginPage = () => {
+   const [email, setEmail] = useState('');
+   const [password, setPassword] = useState('');
+   const [error, setError] = useState('');
+   const [loading, setLoading] = useState(false);
+
+   const handleLogin = async (e) => {
+    e.preventDefault();
+    setError('');
+    setLoading(true);
+
+    try {
+        const userData = await loginUser({email, password});
+        console.log('User logged in:', userData);
+    } catch (err) {
+        setError(err.response?.sata?.message || 'Login failed');
+    } finally {
+        setLoading(false);
+    }
+   }
+};
+
+return (
+    <div className='login-page' style={{maxWidth: 400, margin: 'auto'}}>
+        <h2>LogIn</h2>
+        <form onSubmit={handleLogin}>
+            <input
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+            />
+            <input
+                type="password"
+                palceholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+            />
+            <button type="submit" disabled={loading}>
+                {loading ? 'Logging in...' : 'Log In'}
+            </button>
+            {error && <p style={{color:'red'}}>{error}</p>}
+        </form>
+    </div>
+)
+
+export default LoginPage;
